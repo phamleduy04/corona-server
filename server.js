5e045a92ef285384b614d0d5cc4f5e7af37a1c31
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-02 21:54:42 -0600

    corona-api v1.1.1
    Add thêm Trung Quốc vào list.

--- server.js
+++ server.js
@@ -126,6 +126,21 @@ app.get('/korea', (req, res) => {
     })
 })
 
+app.get('/china', (req, res) => {
+    graphqlclient.request(query).then(result => {
+        var json_data = result.countries.filter(find => find.Country_Region == "Mainland China")
+        var json_data = json_data[0]
+        var timestamp = new Date(parseInt(json_data.Last_Update))
+        var date = timestamp.getDate() + '/' + (timestamp.getMonth() + 1) + '/' + timestamp.getFullYear()
+        let json_response = {
+            "messages": [
+                { "text": `Trung Quốc hiện tại có ${json_data.Confirmed} ca nhiễm, ${json_data.Deaths} ca tử vong và ${json_data.Recovered} ca đã hồi phục. \nNgày cập nhật: ${date}` },
+            ]
+        }
+        res.send(json_response)
+    })
+})
+
 app.get('/news', (req, res) => {
     var push_json = {
         "messages": [{
