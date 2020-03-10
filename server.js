4fcc8dcbeb1b248ba2790ae86ddb60dd5c0189df
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-10 10:37:42 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const url = "https://corona-api.kompa.ai/graphql";
 const graphql = require("graphql-request");
 const NewsAPI = require('newsapi');
-const {news_api_key} = require('./config.json')
+const { news_api_key } = require('./config.json')
 const newsapi = new NewsAPI(news_api_key);
 const query = `query countries {
     countries {
@@ -143,150 +143,73 @@ app.get('/', (req, res) => {
     console.log(req.query)
     res.send("Home page. Server running okay.");
 });
-app.get('/language', (req, res) => {
-    console.log(req.query);
-    let lang = JSON.parse(fs.readFileSync('./lang.json', 'utf8'));
-    if (req.query.lang == "vn") {
-        userid = req.query.userid
-        if (!lang[userid]) {
-            lang[userid] = {
-            lang: "vn"
-        }
-        } else {
-            lang[userid] = {
-                lang: "vn"
-            }
-        }
-        fs.writeFile("./lang.json", JSON.stringify(lang), (err) => {
-            if (err) console.log(err)
-        });
-        var json_response = { "messages": [{ "text": "Đã chọn ngôn ngữ của bạn là Tiếng Việt (Vietnamese)" }] }
-        res.send(json_response)
-    } else if (req.query.lang == "en") {
-        userid = req.query.userid
-        if (!lang[userid]) {
-            lang[userid] = {
-            lang: "en"
-        }
-        } else {
-            lang[userid] = {
-                lang: "en"
-            }
-        }
-        fs.writeFile("./lang.json", JSON.stringify(lang), (err) => {
-            if (err) console.log(err)
-        });
-        var json_response = { "messages": [{ "text": "Your selected language is English" }] }
-        res.send(json_response)
-    }
-})
 
 app.get('/vnfull', (req, res) => {
-    let lang = JSON.parse(fs.readFileSync('./lang.json', 'utf8'));
-    var userid = req.query.userid
-    if (!lang[userid]) lang[userid] = {
-        lang: "vn"
-    }
-    fs.writeFile("./lang.json", JSON.stringify(lang), (err) => {
-        if (err) console.log(err)
-    });
-    if (lang[req.query.userid].lang == 'vn') {
+    if (req.query.lang == 'en') {
         graphqlclient.request(query).then(result => {
-            var json_response = {
-                "messages": []
+            var total = ""
+            result.provinces.forEach(tentp => {
+                var line = `${tentp.Province_Name} currently has ${tentp.Confirmed} confirmed cases, ${tentp.Deaths} deaths cases and ${tentp.Recovered} recoveries cases.\n`
+                total += line  
+            })
+            var response = {
+                "messages": [{ "text": `${total}` }]
             }
+            res.send(response)
+        })
+    } else {
+        graphqlclient.request(query).then(result => {
+            var total = ""
             result.provinces.forEach(tentp => {
                 var response = { "text": `${tentp.Province_Name} hiện tại có ${tentp.Confirmed} ca nhiễm, ${tentp.Deaths} ca tử vong và ${tentp.Recovered} ca hồi phục.` }
-                json_response["messages"].push(response)
+                total += line
             })
-            res.send(json_response)
-        })
-    } else if (lang[req.query.userid].lang == 'en') {
-        graphqlclient.request(query).then(result => {
-            var json_response = {
-                "messages": []
-            }
-            result.provinces.forEach(tentp => {
-                var response = { "text": `${tentp.Province_Name} currently has ${tentp.Confirmed} confirmed cases, ${tentp.Deaths} deaths cases and ${tentp.Recovered} recoveries cases.` }
-                json_response["messages"].push(response)
-            })
-            res.send(json_response)
+            res.send(total)
         })
     }
 })
 
 app.get('/corona', (req, res) => {
     console.log(req.query)
-    let lang = JSON.parse(fs.readFileSync('./lang.json', 'utf8'));
-    var tukhoa = req.query.countries.toLowerCase()
-    var userid = req.query.userid
-    if (!lang[userid]) lang[userid] = {
-        lang: "vn"
-    }
-    fs.writeFile("./lang.json", JSON.stringify(lang), (err) => {
-        if (err) console.log(err)
-    });
-
-    if (lang[req.query.userid].lang == 'vn') {
-        if (search[tukhoa]){
-            graphqlclient.request(query).then(result => {
-                var json_data = result.countries.filter(find => find.Country_Region == search[tukhoa])
-                var json_data = json_data[0]
-                var timestamp = new Date(parseInt(json_data.Last_Update))
-                var date = timestamp.getDate() + '/' + (timestamp.getMonth() + 1) + '/' + timestamp.getFullYear()
-                let json_response = {
-                    "messages": [
-                        { "text": `${search[tukhoa]} hiện tại có ${json_data.Confirmed} ca nhiễm, ${json_data.Deaths} ca tử vong và ${json_data.Recovered} ca đã hồi phục. \nNgày cập nhật: ${date}` },
-                    ]
-                }
-                res.send(json_response)
-            })
-        } else if (tukhoa.length == 2) {
-            let json_response = {
-                "messages": [
-                    { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Bạn phải nhập mã quốc gia 2 chữ để sử dụng tính năng này. Click vào nút ở dưới để tham khảo.", "buttons": [{ "type": "web_url", "url": "https://corona-js.herokuapp.com/countrycode", "title": "Click để vào trang web!" }] } } }
-                ],
-                "text": "Tips: Hãy chú ý tới cột Alpha-2 code nha <3."
-            }
-            res.send(json_response)
-
-        } else if (tukhoa.length == 2 && !search[tukhoa]) {
-            let json_response = {
-                "messages": [{ "text": "Lỗi, không tìm thấy tên đất nước bạn tìm, hoặc nước này hiện tại đang không có dịch corona!" }]
-            }
-            res.send(json_response)
-        }
-
-    } else if (lang[req.query.userid].lang == 'en') {
-        if (search[tukhoa]){
-            graphqlclient.request(query).then(result => {
-                var json_data = result.countries.filter(find => find.Country_Region == search[tukhoa])
-                var json_data = json_data[0]
-                var timestamp = new Date(parseInt(json_data.Last_Update))
-                var date = timestamp.getDate() + '/' + (timestamp.getMonth() + 1) + '/' + timestamp.getFullYear()
+    var tukhoa = req.query.countries
+    if (search[tukhoa.toLowerCase()]) {
+        graphqlclient.request(query).then(result => {
+            var json_data = result.countries.filter(find => find.Country_Region == search[tukhoa])
+            var json_data = json_data[0]
+            var timestamp = new Date(parseInt(json_data.Last_Update))
+            var date = timestamp.getDate() + '/' + (timestamp.getMonth() + 1) + '/' + timestamp.getFullYear()
+            if (req.query.lang.toLowerCase() == 'en') {
                 let json_response = {
                     "messages": [
                         { "text": `${search[tukhoa]} currently has ${json_data.Confirmed} confirmed cases, ${json_data.Deaths} death cases and ${json_data.Recovered} recoveries cases. \nUpdated date: ${date}` },
                     ]
                 }
                 res.send(json_response)
-            })
-        } else if (tukhoa.length == 2) {
-            let json_response = {
-                "messages": [
-                    { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "You must enter a 2-letter country code to use this feature. Click on the button below for reference.", "buttons": [{ "type": "web_url", "url": "https://corona-js.herokuapp.com/countrycode", "title": "Click here." }] } } }
-                ],
-                "text": "Tips: Please pay attention to column Alpha-2 code <3."
+            } else {
+                let json_response = {
+                    "messages": [
+                        { "text": `${search[tukhoa]} hiện tại có ${json_data.Confirmed} ca nhiễm, ${json_data.Deaths} ca tử vong và ${json_data.Recovered} ca đã hồi phục. \nNgày cập nhật: ${date}` },
+                    ]
+                }
+                res.send(json_response)
             }
-            res.send(json_response)
-        } else if (tukhoa.length == 2 && !search[tukhoa]) {
-            let json_response = {
-                "messages": [{ "text": "Error, did not find the name of the country you are looking for, or this country currently has no coronavirus cases!" }]
-            }
-            res.send(json_response)
+        })
+    } else if (tukhoa.length !== 2) {
+        let json_response = {
+            "messages": [
+                { "attachment": { "type": "template", "payload": { "template_type": "button", "text": "Bạn phải nhập mã quốc gia 2 chữ để sử dụng tính năng này. Click vào nút ở dưới để tham khảo.", "buttons": [{ "type": "web_url", "url": "https://corona-js.herokuapp.com/countrycode", "title": "Click để vào trang web!" }] } } }
+            ],
+            "text": "Tips: Hãy chú ý tới cột Alpha-2 code nha <3."
         }
+        res.send(json_response)
+
+    } else if (tukhoa.length == 2 && !search[tukhoa.toLowerCase()]) {
+        let json_response = {
+            "messages": [{ "text": "Lỗi, không tìm thấy tên đất nước bạn tìm, hoặc nước này hiện tại đang không có dịch corona!" }]
+        }
+        res.send(json_response)
     }
-})
+});
 
 app.get('/countrycode', (req, res) => {
     res.redirect('https://www.iban.com/country-codes');
@@ -311,21 +234,21 @@ app.get('/news', (req, res) => {
             pageSize: 10,
             language: 'en',
             country: 'us'
-          }).then(response => {
-                response.articles.forEach(n => {
-                    push_json.messages[0].attachment.payload.elements.push({
-                        "title": n.title,
-                        "image_url": n.urlToImage,
-                        "subtitle": `Source: ${n.source.name}`,
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": n.url,
-                            "title": "Go to website"
-                        }]
-                    })
+        }).then(response => {
+            response.articles.forEach(n => {
+                push_json.messages[0].attachment.payload.elements.push({
+                    "title": n.title,
+                    "image_url": n.urlToImage,
+                    "subtitle": `Source: ${n.source.name}`,
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": n.url,
+                        "title": "Go to website"
+                    }]
                 })
+            })
             res.send(push_json);
-          });
+        });
     } else {
         graphqlclient.request(news_query).then(result => {
             result.topTrueNews.forEach(n => {
