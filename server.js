97e179bf16283f9f9a3b65eeced61bc34b250891
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-23 21:04:37 -0400

    update data + allow origin all website

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const ms = require('ms')
 const fs = require('fs')
 const axios = require("axios");
 const cheerio = require("cheerio");
+const JSAlert = require("js-alert");
 const query = `query countries {
     countries {
         Country_Region
@@ -142,9 +143,14 @@ const graphqlclient = new graphql.GraphQLClient(url, {
 })
 
 var app = express();
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use((req, res) => {
+    bodyParser.urlencoded({
+        extended: false
+})
+    res.header('Access-Control-Allow-Origin', '*');
+
+})
+    
 var server = http.createServer(app);
 
 app.get('/', (req, res) => {
@@ -378,16 +384,20 @@ app.get('/usstates', (req, res) => {
 app.get('/countrycode', (req, res) => {
     res.redirect('https://www.iban.com/country-codes');
 })
-
 app.get('/global', (req, res) => {
     var data = JSON.parse(fs.readFileSync('./total.json'))
     if (req.query.lang == 'en'){
+        var text_res = `Global currently has ${data.Global_Cases} total cases, ${data.Global_Deaths} deaths cases, ${data.Global_Recovered} recovered cases`
+        if (req.query.alert == 'true'){
+            res.send(JSAlert.alert("test"))
+        } else {
         let json_response = {
             "messages": [
-                { "text": `Global currently has ${data.Global_Cases} total cases, ${data.Global_Deaths} deaths cases, ${data.Global_Recovered} recovered cases`},
+                { "text": text_res},
             ]
         }
         res.send(json_response)
+        }
     } else {
         let json_response = {
             "messages": [
