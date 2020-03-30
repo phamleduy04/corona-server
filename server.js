a2712a22972bda112383de91f98238c8e816694e
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-03-29 22:30:07 -0500

    update data + release corona-api (v4.0.0)

--- server.js
+++ server.js
@@ -1,49 +1,24 @@
-var http = require('http');
-var bodyParser = require('body-parser');
-var express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const express = require('express');
 const getJSON = require('get-json');
 const capitalize = require('capitalize');
-var UsaStates = require('usa-states').UsaStates;
-const url = "https://corona-api.kompa.ai/graphql";
-const arcgis_url = 'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&outSR=102100&resultOffset=0&resultRe%20cordCount=160&cacheHint=true'
-const worldometers_url = 'https://www.worldometers.info/coronavirus/'
-const us_state_url = 'https://www.worldometers.info/coronavirus/country/us/'
-const graphql = require("graphql-request");
+const UsaStates = require('usa-states').UsaStates;
 const NewsAPI = require('newsapi');
 const { news_api_key } = require('./config.json');
-const newsbreak_url = 'https://www.newsbreak.com/topics/coronavirus'
 const newsapi = new NewsAPI(news_api_key);
 const ms = require('ms');
 const stringsimilarity = require('string-similarity');
 const fs = require('fs');
-var usprovincelist = fs.readFileSync('./listprovince.txt', 'utf8').split(',')
-const axios = require("axios");
-const cheerio = require("cheerio");
-const query = `query countries {
-    countries {
-        Country_Region
-        Confirmed
-        Deaths
-        Recovered 
-        Last_Update
-    }
-    provinces {
-        Province_Name
-        Province_Id
-        Confirmed
-        Deaths
-        Recovered
-        Last_Update
-    }
-}`;
-const news_query = `query topTrueNews {
-    topTrueNews {
-        title
-        url
-        siteName
-        picture
-        }
-}`
+const usprovincelist = fs.readFileSync('./listprovince.txt', 'utf8').split(',')
+//url list
+const arcgis_url = 'https://viruscoronaapi.herokuapp.com/arcgis'
+const kompa_news_url = 'https://viruscoronaapi.herokuapp.com/kompa?data=news'
+const kompa_vnfull = 'https://viruscoronaapi.herokuapp.com/kompa'
+const worldometers_url = 'https://viruscoronaapi.herokuapp.com/worldometers'
+const worldometers_total_url = 'https://viruscoronaapi.herokuapp.com/worldometers?data=total'
+const worldometers_usstate_url = 'https://viruscoronaapi.herokuapp.com/worldometers?data=usstate'
+const newsbreak_url = 'https://viruscoronaapi.herokuapp.com/breaknews'
 const search = {
     "ad": "Andorra",
     "af": "Afghanistan",
@@ -133,22 +108,6 @@ const search = {
     "vn": "Vietnam"
 }
 
-const graphqlclient = new graphql.GraphQLClient(url, {
-    headers: {
-        Authority: "corona-api.kompa.ai",
-        Scheme: "https",
-        Path: "/graphql",
-        Accept: "*/*",
-        UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-        Origin: "https://corona.kompa.ai",
-        secfetchsize: "same-site",
-        secfetchmode: "cors",
-        Referer: "https://corona.kompa.ai",
-        AcceptEncoding: "gzip, deflate, br",
-        AcceptLanguage: "vn-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5"
-    },
-})
-
 var app = express();
 app.use(bodyParser.urlencoded({
     extended: false
@@ -165,110 +124,54 @@ app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 });
 
-setInterval(async function() { //wordometers
-            console.time('worldometer')
-            let Countries = []
-            const result = await axios.get(worldometers_url);
-            let $ = cheerio.load(result.data);
-            $('#main_table_countries_today').find('tbody').eq(0).find('tr').each((i, el) => {
-                Countries.push($(el).find('td').eq(0).text().trim())
-            })
-
-            const json_response = []
-            Countries.forEach(country => {
-                        let Index = Countries.indexOf(country) + 1
-                        json_response.push({
-                                    Country_Name: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(1)`).text().trim() || '0'}`,
-            Total_Cases: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(2)`).text().trim() || '+0'}`,
-            New_Cases: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(3)`).text().trim() || '0'}`,
-            Total_Deaths: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(4)`).text().trim() || '0'}`,
-            New_Deaths: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(5)`).text().trim() || '+0'}`,
-            Total_Recovered: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(6)`).text().trim() || '0'}`,
-            Serious_Cases: `${$(`#main_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(8)`).text().trim() || '0'}`
-            })
-        })
-            fs.writeFileSync('./worldometers.json',JSON.stringify(json_response))
-            await console.log('Đã ghi file worldometers.json')
-            console.timeEnd('worldometer')
-            // Total của worldometers
-            console.time('worldometer-total')
-            var data = $('.maincounter-number').text().trim()
-            var data = data.replace(/\s\s+/g, ' ').split(' ');
-            let total_json = {
-                Global_Cases: data[0],
-                Global_Deaths: data[1],
-                Global_Recovered: data[2]
-            }
-            fs.writeFileSync('./total.json', JSON.stringify(total_json))
-            await console.log('Đã ghi file total.json')
-            console.timeEnd('worldometer-total')
-            //arcgis url
-            console.time('arcgis')
-            getJSON(arcgis_url).then(async response => {
-                if (response.error) {
-                    return console.log('Error!');
-                } else {
-                    fs.writeFileSync('./arcgis.json', JSON.stringify(response))
-                    await console.log('Đã ghi file arcgis.json')
-                }
-            })
-            console.timeEnd('arcgis')
-            //us state
-            console.time('worldometer-usstate')
-            let US_STATE = []
-            const usstateresult = await axios.get(us_state_url);
-            let $state = cheerio.load(usstateresult.data);
-            $state('#usa_table_countries_today').find('tbody').eq(0).find('tr').each((i, el) => {
-                US_STATE.push($state(el).find('td').eq(0).text().trim())
-            })
-        
-            let us_state_json = []
-                    US_STATE.forEach(state => {
-                        let Index = US_STATE.indexOf(state) + 1
-                        us_state_json.push({
-                            State_Name: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(1)`).text().trim() || '0'}`,
-            Total_Cases: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(2)`).text().trim() || '+0'}`,
-            New_Cases: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(3)`).text().trim() || '0'}`,
-            Total_Deaths: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(4)`).text().trim() || '0'}`,
-            New_Deaths: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(5)`).text().trim() || '+0'}`,
-            Total_Recovered: `N/A`
-            })
-            })
-            //Total_Recovered: `${$state(`#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(${Index}) > td:nth-child(6)`).text().trim() || '0'}`
-            fs.writeFileSync('./us.json', JSON.stringify(us_state_json))
-            console.log('Đã ghi file us.json')
-            console.timeEnd('worldometer-usstate')
-}, ms('5m'))
-async function newsbreak() {
-    console.time('newsbreak');
-    let us_full_json = []
-    const result = await axios.get(newsbreak_url)
-    let $ = cheerio.load(result.data)
-    var i_max =  $('.state-table').find('tbody').eq(0).find('span').length
-    for (i = 1; i < i_max; i++){
-        var confirmed_arr = $(`.state-table > tbody > tr:nth-child(${i}) > td:nth-child(2)`).text().trim().split('+')
-        var death_arr = $(`.state-table > tbody > tr:nth-child(${i}) > td:nth-child(3)`).text().trim().split('+')
-        var name = $(`.state-table > tbody > tr:nth-child(${i})`).find('span').text().trim()
-        var confirmed = confirmed_arr[0];
-        var new_confirmed = confirmed_arr[1] || 0;
-        var deaths = death_arr[0];
-        var new_deaths = death_arr[1] || 0;
-        if (!name) continue;
-        if (name.startsWith('▸')) continue;
-        us_full_json.push({
-            Province_Name: `${name}`,
-            Confirmed: `${confirmed}`,
-            New_Confirmed: `${new_confirmed}`,
-            Deaths: `${deaths}`,
-            New_Deaths: `${new_deaths}`
-        })
-        console.log(name)
-    }
-    fs.writeFileSync('./usprovince.json', JSON.stringify(us_full_json))
-    console.log('Done writing data to usprovince.json')
-    console.timeEnd('newsbreak')
-}
-setInterval(newsbreak, ms('30m')) //news break (getdata avg time: 154823.547ms ~ 2.5 min)
+setInterval(async function() { 
+    console.time('start')
+    //arcgis
+    getJSON(arcgis_url, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./arcgis.json', JSON.stringify(response))
+        console.log('Đã ghi file arcgis.json')
+    })
+    //kompa news
+    getJSON(kompa_news_url, function(error, response) {
+        if (error) return;
+        fs.writeFileSync('./kompa_news.json', JSON.stringify(response))
+        console.log('Đã ghi file arcgis.json')
+    })
+    //kompa (vnfull)
+    getJSON(kompa_vnfull, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./vnfull.json', JSON.stringify(response))
+        console.log('Đã ghi file vnfull.json')
+    })
+    //worldometers
+        //global
+    getJSON(worldometers_url, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./worldometers.json', JSON.stringify(response))
+        console.log('Đã ghi file worldometers.json')
+    })
+        //total
+    getJSON(worldometers_total_url, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./total.json', JSON.stringify(response))
+        console.log('Đã ghi file total.json')
+    })
+        //us state
+    getJSON(worldometers_usstate_url, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./us.json', JSON.stringify(response))
+        console.log('Đã ghi file us.json')
+    })
+    //newsbreak
+    getJSON(newsbreak_url, function(error, response){
+        if (error) return;
+        fs.writeFileSync('./usprovince.json', JSON.stringify(response))
+        console.log('Đã ghi file usprovince.json')
+    })
+    console.log('Đã ghi hết tất cả file')
+    console.timeEnd('start')
+}, ms('3m'))
 
 app.get('/cansearch', (req, res) => {
     var canada_provinces = ["British Columbia", "Ontario", "Alberta", "Quebec", "New Brunswick", "Saskatchewan", "Manitoba", "Nova Scotia", "Grand Princess", "Newfoundland and Labrador", "Prince Edward Island"]
@@ -340,7 +243,19 @@ app.get('/usprovince', (req, res) => {
             res.send(response_json)
         }
     } else {
-        res.send('Invalid')
+        if (req.query.lang == 'en') {
+            var json_response = {
+                "messages": [{ "text": `You must enter a valid US province name, list supported US province name: https://corona-js.herokuapp.com/usprovincewiki`}]
+            }
+            res.send(json_response)
+        } else {
+            var json_response = {
+                "messages": [
+                    { "text": "Bạn phải nhập tên hợp lệ quận của Hoa Kì, list bot đang hỗ trợ: https://corona-js.herokuapp.com/usprovincewiki"}
+                ]
+            }
+            res.send(json_response)
+        }
     }
 })
 
@@ -428,30 +343,27 @@ app.get('/ussearch', (req, res) => {
 })
 
 app.get('/vnfull', (req, res) => {
+    let data = JSON.parse(fs.readFileSync('vnfull.json'))
     if (req.query.lang == 'en') {
-        graphqlclient.request(query).then(result => {
-            var total = ""
-            result.provinces.forEach(tentp => {
-                var line = `${tentp.Province_Name} currently has ${tentp.Confirmed} confirmed cases, ${tentp.Deaths} deaths cases and ${tentp.Recovered} recoveries cases.\n\n`
-                total += line
-            })
-            var response = {
-                "messages": [{ "text": `${total}` }]
-            }
-            res.send(response)
+        var total = ""
+        data.provinces.forEach(tentp => {
+            var line = `${tentp.Province_Name} currently has ${tentp.Confirmed} confirmed cases, ${tentp.Deaths} deaths cases and ${tentp.Recovered} recoveries cases.\n\n`
+            total += line
         })
+        var response = {
+            "messages": [{ "text": `${total}` }]
+        }
+        res.send(response)
     } else {
-        graphqlclient.request(query).then(result => {
-            var total = ""
-            result.provinces.forEach(tentp => {
-                var line = `${tentp.Province_Name} hiện tại có ${tentp.Confirmed} ca nhiễm, ${tentp.Deaths} ca tử vong và ${tentp.Recovered} ca hồi phục.\n\n`
-                total += line
-            })
-            var response = {
-                "messages": [{ "text": `${total}` }]
-            }
-            res.send(response)
+        var total = ""
+        data.provinces.forEach(tentp => {
+            var line = `${tentp.Province_Name} hiện tại có ${tentp.Confirmed} ca nhiễm, ${tentp.Deaths} ca tử vong và ${tentp.Recovered} ca hồi phục.\n\n`
+            total += line
         })
+        var response = {
+            "messages": [{ "text": `${total}` }]
+        }
+        res.send(response)
     }
 })
 
@@ -589,23 +501,22 @@ app.get('/news', (req, res) => {
             }]
         }
         if (countries == 'vn') {
-            graphqlclient.request(news_query).then(result => {
-                result.topTrueNews.forEach(n => {
-                    if (n.title.length > 0 && n.picture !== null && n.siteName.length > 0 && n.url.length > 0) {
-                        push_json.messages[0].attachment.payload.elements.push({
-                            "title": n.title,
-                            "image_url": n.picture,
-                            "subtitle": `Nguồn: ${n.siteName}`,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": n.url,
-                                "title": "Đọc báo"
-                            }]
-                        })
-                    }
-                })
-                res.send(push_json)
+            let data = JSON.parse(fs.readFileSync('./kompa_news.json'))
+            data.topTrueNews.forEach(n => {
+                if (n.title.length > 0 && n.picture !== null && n.siteName.length > 0 && n.url.length > 0) {
+                    push_json.messages[0].attachment.payload.elements.push({
+                        "title": n.title,
+                        "image_url": n.picture,
+                        "subtitle": `Nguồn: ${n.siteName}`,
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": n.url,
+                            "title": "Đọc báo"
+                        }]
+                    })
+                }
             })
+            res.send(push_json)
         } else {
             newsapi.v2.topHeadlines({
                 q: 'coronavirus',
