121b432d7fc9c6404d0b0c124209a4689a4dae71
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-04-03 18:48:13 -0500

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-async function main(){
-    var response = JSON.parse(fs.readFileSync('./data/arcgis.json'))
-    var state = response.features.filter(m => m.attributes.Country_Region == "Australia")
-    console.log(state)
-};
-main();
